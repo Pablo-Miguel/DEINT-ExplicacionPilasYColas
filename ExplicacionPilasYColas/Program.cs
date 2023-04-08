b7a1db938c5417612c1bbaaf1913bcc14cf6b80b
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-04-08 13:47:43 +0200

    Agregar archivos de proyecto.

---
+++ ExplicacionPilasYColas/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//PILA
+
+Stack<int> enteros = new Stack<int>();
+
+enteros.Push(1);
+enteros.Push(2);
+enteros.Push(3);
+
+enteros.Pop();
+
+enteros.Count();
+
+//COLA
+
+Queue<int> colaenteros = new Queue<int>();
+
+colaenteros.Enqueue(1); //Introduce el valor a la cola;
+colaenteros.Enqueue(2);
+colaenteros.Enqueue(3);
+
+colaenteros.Dequeue();
+
+
+Console.WriteLine();
+
